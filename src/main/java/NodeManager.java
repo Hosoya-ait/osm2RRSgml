18e7d5ef90aece3a9d1e4908576e73ec77a3dccc
k14105kk <k14105kk@aitech.ac.jp>
2017-10-14 17:47:32 +0900

    新クラス構造案通りに大体の処理と変数を書いた. node_idの引数の問題 addOsmNodeの後始末問題が残っている. 詳細はコメントへ

--- src/main/java/NodeManager.java
+++ src/main/java/NodeManager.java
@@ -1,2 +1,71 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class NodeManager {
+    // ******  要相談事項  ******
+    //node_id_をintにしているため，メソッドにて引数の問題が随所で発生している.
+    //addGmlNodeとaddOsmNodeについて，
+    //  osmIDではなくgmlでのnodeIDでxy座標を管理するように変更したため，
+    //  addOsmNodeメソッドの後始末をどうするか.
+
+
+    //gmlに書き込むnodeIDとx、yの値の対応付け
+    private static HashMap<String,HashMap<String,Double>> gml_node_map_ = new HashMap<String,HashMap<String,Double>>();
+    //osmIDからnodeIDへの対応
+    private static HashMap<String,String> osm_to_gml_id_ = new HashMap<String,String>();
+    //nodeIDからosmIDへの対応
+    private static HashMap<String,String> gml_to_osm_id_ = new HashMap<String,String>();
+    //gmlに書き込むnodeIDを一時的に保持
+    private static int node_id_ = 1;
+    //edgeで使用したnodeを保持
+    private static ArrayList<String> used_node_list_ = new ArrayList<String>();
+
+    //nodeIDからosmIDを取得  引数の型を調べる
+    private static String getOsmID(int node_id){
+        return gml_to_osm_id_.get(node_id);
+    }
+    //osmIDからnodeIDを取得　引数の型を調べる
+    private static String getGmlID(int osm_id){
+        return osm_to_gml_id_.get(osm_id);
+    }
+    //nodeIDの示すnodeのx座標
+    private static double getX(int node_id){
+        return gml_node_map_.get(node_id).get("x");
+    }
+    //nodeIDの示すnodeのy座標
+    private static double getY(int node_id){
+        return gml_node_map_.get(node_id).get("y");
+    }
+    //nodeがすでに使用されているか調べてboolを返す
+    private static boolean checkUsedNodeList(int node_id){
+        if(used_node_list_.contains(node_id)){
+            return true;
+        }
+        return false;
+    }
+
+    //gml用のnodeをnodeIDと共に追加
+    //引数の型を合わせないと　考えるのはあとで　node_id_の++忘れない
+    //osm_idとxyをマップしないことになったため，ここでosmIDとnodeIDを合わせることにした
+    private static int addGmlNode(String osm_id,HashMap nodeXY){
+        node_id_ += 1;
+        gml_node_map_.put(node_id_, nodeXY);
+
+        osm_to_gml_id_.put(osm_id, node_id_);
+        gml_to_osm_id_.put(node_id_, osm_id);
+
+        return node_id_;
+    }
+    //osmのあるnodeのx,yの座標情報をnodeIDに流すはずだったが，
+    //新クラス構造案にてxy管理はgmlNodeMapのみなので、IDの関連付けのみが残る
+    //中身の処理は”addGmlNode”メソッドに移した↑
+    private static void addOsmNode(String osm_id){
+
+    }
+    //nodeを使用済みとみなす
+    //引数の型をintのままにするか
+    private static void setUsedNodeList(int node_id){
+        used_node_list_.add(node_id);
+    }
+
 }
