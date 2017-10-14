bbcb8b5604ae3ac6871f2f3c4548a2a6f42a1b3f
k14105kk <k14105kk@aitech.ac.jp>
2017-10-14 20:41:38 +0900

    新クラス構造通りに設計

--- src/main/java/HighwayManager.java
+++ src/main/java/HighwayManager.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class HighwayManager {
+    private static ArrayList<ArrayList<String>> tmpHighwayList = new ArrayList<ArrayList<String>>();
+
+    public static ArrayList getTmpHighwayList() {
+        return tmpHighwayList;
+    }
+
+    public static void setTmpHighwayList(ArrayList highwayNodes) {
+        tmpHighwayList.add(highwayNodes);
+    }
 }
