59a44b11b44eeabda4f45fa523e324346382e466
k14105kk <k14105kk@aitech.ac.jp>
2017-10-15 18:01:13 +0900

    NodeManagerの一通りの動作の確認のtestクラス作成しました. NodeManagerの一部メソッドの引数の型が間違ってたので修正. 動作は完璧！

---
+++ src/main/java/Test_NodeManager.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Test_NodeManager {
+    public static void main(String[] args){
+        //addGmlNodeメソッドのテスト
+        HashMap<String,Double> test_map = new HashMap<String,Double>();
+        test_map.put("x",300.0);
+        test_map.put("y",400.0);
+        String node_id = "1";
+        String osm_id  = "3";
+        NodeManager.addGmlNode(osm_id,test_map);
+
+        System.out.println("付与したnodeID：" + NodeManager.getGmlID(osm_id));   //1
+        System.out.println("付与したosmID：" + NodeManager.getOsmID(node_id));  //3
+        System.out.println("登録したx座標：" + NodeManager.getX(node_id));      //300.0
+        System.out.println("登録したy座標：" + NodeManager.getY(node_id));      //400.0
+
+        String osm_id2 = "4";
+        NodeManager.addGmlNode(osm_id2,test_map);
+        System.out.println("2回目のnode追加でnodeIDが＋１されているか確認: " + NodeManager.getGmlID(osm_id2)); //2
+
+
+        //setUsedNodeListメソッドのテスト
+        NodeManager.setUsedNodeList(osm_id);
+        System.out.println("trueが返ってたらsetUsedNodeList機能してる : " + NodeManager.checkUsedNodeList(osm_id)); //true
+    }
+}
