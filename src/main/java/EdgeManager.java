faec58944607c0979e926b767b81dcf74a8d42e9
k14105kk <k14105kk@aitech.ac.jp>
2017-10-25 21:24:20 +0900

    edge_id_の動作の確認を行い，値の初期値と増加部分が適切になるように修正 インデントを一部修正 checkMinusDirectionEdge()のif文の判定が間違っていたので，正しく動くように修正

--- src/main/java/EdgeManager.java
+++ src/main/java/EdgeManager.java
@@ -5,20 +5,19 @@ public class EdgeManager {
     //新しいedgeIDと新しいnodeIDを対応付ける
     private static HashMap<String,ArrayList<String>> edge_map_ = new HashMap<String,ArrayList<String>>();
     //新しいedgeID クラス内部ではint，外部ではStringで扱う
-    private static int edge_id_ = 1;
+    private static int edge_id_ = 0;
 
-    public static String getEdgeID() {
-        return String.valueOf(edge_id_);
-    }
+    public static String getEdgeID() { return String.valueOf(edge_id_); }
     //edge_id_で管理している2点のNodeのArrayを返す
-    public static ArrayList getEdgeNodeList(String edge_id) {
-        return edge_map_.get(edge_id);
-    }
+    public static ArrayList getEdgeNodeList(String edge_id) { return edge_map_.get(edge_id); }
 
-    //引数の2点を利用したedgeが他にあれば，マイナスにする処理のためにtrue返す
+    //2点のosmIDを利用したedgeが他にあれば，マイナスにする処理のためにtrue返す
+    //これedgeを構成するnodeのArrayを引数にした方が扱いやすいのでは？
     public static boolean checkMinusDirectionEdge(String nodeA,String nodeB) {
-        for (int i=1; edge_map_.size()>i; i++) {
-            if (edge_map_.get(i).contains(nodeA) && edge_map_.get(i).contains(nodeB)) {
+        //edge_map_のkeyは1から始まり，edge_map_.size()と同じ値までのidを保持していることに注意
+        for (int i=1; edge_map_.size()>=i; i++) {
+            String edge_id = String.valueOf(i);
+            if (edge_map_.get(edge_id).contains(nodeA) && edge_map_.get(edge_id).contains(nodeB)) {
                 return true;
             }
         }
@@ -26,7 +25,8 @@ public class EdgeManager {
     }
 
     //edgeを構成する2つの新しいnodeIDを渡して, 新しいedgeIDと対応させる
-    public static void setEdgeMap(ArrayList nodes) {
-        edge_map_.put(String.valueOf(edge_id_++), nodes);
+    public static void setEdgeMap(ArrayList new_nodes) {
+        edge_id_++;
+        edge_map_.put(String.valueOf(edge_id_), new_nodes);
     }
 }
