0a520f1ef85911a25ede5e13c4dbd015d075a429
k14105kk <k14105kk@aitech.ac.jp>
2017-10-25 19:27:44 +0900

    node_idの取得している様をわかりやすくした

--- src/main/java/Test_NodeManager.java
+++ src/main/java/Test_NodeManager.java
@@ -6,11 +6,11 @@ public class Test_NodeManager {
         HashMap<String,Double> test_map = new HashMap<String,Double>();
         test_map.put("x",300.0);
         test_map.put("y",400.0);
-        String node_id = "1";
         String osm_id  = "3";
         NodeManager.addGmlNode(osm_id,test_map);
-
         System.out.println("付与したnodeID：" + NodeManager.getGmlID(osm_id));   //1
+
+        String node_id = NodeManager.getGmlID(osm_id);
         System.out.println("付与したosmID：" + NodeManager.getOsmID(node_id));  //3
         System.out.println("登録したx座標：" + NodeManager.getX(node_id));      //300.0
         System.out.println("登録したy座標：" + NodeManager.getY(node_id));      //400.0
