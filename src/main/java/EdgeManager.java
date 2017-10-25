7ee30864e4124b1a41a9e3b9039ece848d3b0c0c
k14105kk <k14105kk@aitech.ac.jp>
2017-10-25 19:44:42 +0900

    コメントをつけてわかりやすくした インデントも調整した

--- src/main/java/EdgeManager.java
+++ src/main/java/EdgeManager.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class EdgeManager {
-    //edge_id_は　クラス内部ではint，外部ではStringで扱う
+    //新しいedgeIDと新しいnodeIDを対応付ける
     private static HashMap<String,ArrayList<String>> edge_map_ = new HashMap<String,ArrayList<String>>();
+    //新しいedgeID クラス内部ではint，外部ではStringで扱う
     private static int edge_id_ = 1;
 
     public static String getEdgeID() {
@@ -13,19 +14,19 @@ public class EdgeManager {
     public static ArrayList getEdgeNodeList(String edge_id) {
         return edge_map_.get(edge_id);
     }
-    //引数の2点を利用したedgeが他にあれば，マイナスにする処理のためにfalse返せばいいかな？　確認求む
+
+    //引数の2点を利用したedgeが他にあれば，マイナスにする処理のためにtrue返す
     public static boolean checkMinusDirectionEdge(String nodeA,String nodeB) {
-        for(int i = 1; edge_map_.size()>i; i++){
-            if(edge_map_.get(i).contains(nodeA)
-                    && edge_map_.get(i).contains(nodeB)){
+        for (int i=1; edge_map_.size()>i; i++) {
+            if (edge_map_.get(i).contains(nodeA) && edge_map_.get(i).contains(nodeB)) {
                 return true;
             }
         }
         return false;
     }
 
+    //edgeを構成する2つの新しいnodeIDを渡して, 新しいedgeIDと対応させる
     public static void setEdgeMap(ArrayList nodes) {
         edge_map_.put(String.valueOf(edge_id_++), nodes);
     }
-
 }
