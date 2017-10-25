34cddc50b71074b6ea84d673e8fa5293fbfc5bb1
k14105kk <k14105kk@aitech.ac.jp>
2017-10-25 21:54:55 +0900

    HighwayManagerの動作確認用testクラスを作成

---
+++ src/main/java/Test_HighwayManager.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Test_HighwayManager {
+    public static void main(String[] args) {
+        //highway作成
+        ArrayList<String> test_highway = new ArrayList<String>();
+        test_highway.add("1");
+        test_highway.add("2");
+        test_highway.add("3");
+
+        //highwayをセット
+        HighwayManager.setTmpHighwayList(test_highway);
+
+        //highwayを呼び出す
+        ArrayList<ArrayList<String>> test_get_highway = new ArrayList<ArrayList<String>>();
+        test_get_highway = HighwayManager.getTmpHighwayList();
+        System.out.println(test_get_highway.get(0).get(0));
+        System.out.println(test_get_highway.get(0).get(1));
+        System.out.println(test_get_highway.get(0).get(2));
+
+    }
+}
