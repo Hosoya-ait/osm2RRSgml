e8b553293e84a898ebb97773d8de1461a39ed85a
k15023 <k15023@maslab.aitech.ac.jp>
2018-07-16 00:10:46 +0000

    建物がかぶっている場合に建物の大きさを変更するファイル（座標まで求めた)

--- src/main/java/BuildingCheck.java
+++ src/main/java/BuildingCheck.java
@@ -42,10 +42,10 @@ public class BuildingCheck{
       //System.out.println("int" + Integer.parseInt(tmp));
       for (int k=1; k<= Integer.parseInt(tmp); k++) {
         //building listから初期の値を取得する
-        System.out.println("k= " + k);
+        System.out.println("現在の建物= " + k);
         for (int j=0; j<bm.getBuildingEdgeList(String.valueOf(k)).size(); j++) {
           int hrefValue = nm.getNodeSize() + Integer.parseInt((String)bm.getBuildingEdgeList(String.valueOf(k)).get(j));
-          System.out.println("値 " + hrefValue);
+          System.out.println("値1 " + hrefValue);
 
           //上で取得された値を利用し（hrefValue）取得した値を使用してgml:idから座標を導くidを取得する
           String set_edge_ID = ""+(hrefValue);
@@ -54,6 +54,13 @@ public class BuildingCheck{
           l = l+1;
           ArrayList<String> write_gml_edge = em.getEdgeNodeList(String.valueOf(l));
           System.out.println("値3= " + write_gml_edge.get(0));
+          String num = write_gml_edge.get(0);
+
+          double nm_x = nm.getX(String.valueOf(num));
+          double nm_y = nm.getY(String.valueOf(num));
+
+
+          System.out.println("x座標" +  String.format("%3.3f",nm_x) + " y座標" + String.format("%3.3f",nm_y));
         }//l46
       }
     }
